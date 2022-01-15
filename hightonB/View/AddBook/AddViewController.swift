4103e73dd1b4226d99090e410f58ebb72f7c0a8c
Taeh83 <dreamsky83@naver.com>
2022-01-15 17:19:27 +0900

    fin ui

--- hightonB/View/AddBook/AddViewController.swift
+++ hightonB/View/AddBook/AddViewController.swift
@@ -9,21 +9,34 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var bookName: UITextField!
+    @IBOutlet weak var bookWriter: UITextField!
+    @IBOutlet weak var bookPrice: UITextField!
+    
+    @IBAction func addBooks(_ sender: Any) {
+        //하나라도 비어있으면 입력얼럿, 아니라면 신청완료 얼럿 띄워주기
+        
+        //여기에서 신청한 북 데이터는 첫번째 페이지에 저장된다. ( 여기서 서버에 저장을 해서 그 데이터를 불러오는 건지는 모름 )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addSome()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addSome() {
+        bookName.layer.borderWidth = 0.5
+        bookName.layer.borderColor = UIColor.blue.cgColor
+        bookName.layer.cornerRadius = 20
+        
+        bookWriter.layer.borderWidth = 0.5
+        bookWriter.layer.borderColor = UIColor.blue.cgColor
+        bookWriter.layer.cornerRadius = 20
+        
+        bookPrice.layer.borderWidth = 0.5
+        bookPrice.layer.borderColor = UIColor.blue.cgColor
+        bookPrice.layer.cornerRadius = 20
     }
-    */
-
+    
 }
--- hightonB/View/BookList/BookViewController.swift
+++ hightonB/View/BookList/BookViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class BookViewController: UIViewController {
 
+    let bookLists : [String] = [] //서버에 저장된? bookData를 끌고와서 저장할 배열
+    
+    @IBOutlet weak var listTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        listTable.delegate = self
+        listTable.dataSource = self
+    }
+
+}
+
+extension BookViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = listTable.dequeueReusableCell(withIdentifier: "books", for : indexPath)
+        cell.textLabel?.text = "test text is already!!"
+        return cell
     }
-    */
-
+    
 }
